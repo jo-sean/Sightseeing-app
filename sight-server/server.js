8d33d64ae7223eb78b414b3d41b86d710dc2bba0
Sean Perez <joseanmanuel@gmail.com>
2021-11-26 12:37:18 -0800

    server.js updates

--- sight-server/server.js
+++ sight-server/server.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const cors = require('cors')
-const resultSights = require('./resultSights')
+const express = require('express');
+const cors = require('cors');
+const resultSights = require('./resultSights');
 
-const port = 8222
-const app = express()
+const port = 8222;
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(resultSights);
 
 app.listen(port, () => {
-    console.log(`Server online at http://localhost:${port}`)
-})
+    console.log(`Server online at http://localhost:${port}`);
+});
