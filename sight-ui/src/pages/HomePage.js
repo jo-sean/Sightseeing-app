5fc2f32ddb7d190aeaef5d33df85fb74f88db704
Sean Perez <joseanmanuel@gmail.com>
2021-11-26 12:38:29 -0800

    HomePage updates

--- sight-ui/src/pages/HomePage.js
+++ sight-ui/src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import banner from './media/columbia_rv.jpg'
+import banner from './media/columbia_rv.jpg';
 
 class HomePage extends React.Component {
 
@@ -9,12 +9,12 @@ class HomePage extends React.Component {
             <>
                 <img src={banner} alt="Banner" class='responsive'></img>
 
-                <p class="Intro"> Welcome! This app provides you a list of sightseeing places close to you. To use it,
+                <p class="intro"> Welcome! This app provides you a list of sightseeing places close to you. To use it,
                     click the "Get Started" button below and on the following page put the 5-digit Oregon postal code in the
                     search bar and click the "Search" button'. If you want to do a new zip code, just change it and click "Search"
-                    again. <span className='Warning'> If nothing happens, that means that the postal code entered is not valid. </span> Happy searching!  </p>
+                    again. <span className='warning'> If nothing happens, that means that the postal code entered is not valid. </span> Happy searching!  </p>
 
-                <button className='Button'><Link to="/show-sights" className=' Anchor'>Get Started</Link></button>
+                <button className='button'><Link to="/show-sights" className=' anchor'>Get Started</Link></button>
 
                 <br></br>
                 <br></br>
